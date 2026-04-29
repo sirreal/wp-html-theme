b9b3ee214161b49b56db512e3c883a8283e8275f
Jon Surrell <sirreal@users.noreply.github.com>
2026-04-29 23:41:41 +0200

    Add index.php fallback template (post list)

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Fallback template. Used when no more specific template exists.
+ *
+ * @package HTML
+ */
+
+get_header();
+?>
+
+<?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+        <article>
+            <header>
+                <h1>
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h1>
+                <p>
+                    <time datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>">
+                        <?php echo esc_html( get_the_date() ); ?>
+                    </time>
+                </p>
+            </header>
+            <?php the_excerpt(); ?>
+        </article>
+    <?php endwhile; ?>
+
+    <?php
+    the_posts_pagination(
+        array(
+            'aria_label' => esc_attr__( 'Posts', 'html' ),
+        )
+    );
+    ?>
+<?php else : ?>
+    <article>
+        <h1><?php esc_html_e( 'Nothing here', 'html' ); ?></h1>
+        <p><?php esc_html_e( 'No posts found.', 'html' ); ?></p>
+        <?php get_search_form(); ?>
+    </article>
+<?php endif; ?>
+
+<?php get_footer();
