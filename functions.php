8bd8c861d35c49ebebbd32dc48646e20f75b9df8
Jon Surrell <sirreal@users.noreply.github.com>
2026-04-29 23:39:15 +0200

    Add theme bootstrap, setup, and dequeues

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * HTML theme bootstrap.
+ *
+ * @package HTML
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+require_once get_template_directory() . '/inc/setup.php';
+require_once get_template_directory() . '/inc/class-html-walker-nav-menu.php';
